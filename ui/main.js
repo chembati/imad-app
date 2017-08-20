cf0b59cdda1c9348a2da40cf9c2e3867cde1e5c6
chembati <31111585+chembati@users.noreply.github.com>
2017-08-20 21:30:35 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -17,3 +17,32 @@ function moveRight() {
     marginLeft = marginLeft + 1;
     img.style.marginLeft = marginLeft + 'px';
 }
+
+
+//Counter code
+
+var button = document.getElementById('counter');
+button.onclick = function() {
+    
+    // Create a request
+    var request = XMLHttpRequest();
+    
+    // Make a request
+    request.open('GET','http://sivachembati.imad.hasura-app.io/counter',true);
+    request.send(null);
+    
+    // Capure the response and store it in a variable
+    request.onreadystatechange = function() {
+        if(request.readyState == XMLHttpRequest.DONE) {
+            if(request.response == 200) {
+                var counter = request.responseText;
+                var span = document.getElementById('count');
+                span.innerHTML = counter.toString();
+            }
+        }
+    }
+    // Render the variable in the correct span
+    counter = counter + 1;
+    var span = document.getElementById('count');
+    span.innerHTML = counter.toString();
+}
