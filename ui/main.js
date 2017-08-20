337805f189cfe43793f4b42df5266c84bc0e476f
chembati <31111585+chembati@users.noreply.github.com>
2017-08-20 23:43:02 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -46,3 +46,20 @@ button.onclick = function() {
     var span = document.getElementById('count');
     span.innerHTML = counter.toString();
 }
+
+//Submit name
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
+var submit = document.getElementById('submit_btn');
+submit.onclick = function() {
+    // Make a request to a server and and send the name
+    
+    // Capture the list of names and render it as a list
+    var names = ['name1','name2','name3','name4'];
+    var list = '';
+    for(var i=0;i<<names.length;i++) {
+        list += '<li>' + names[i] + '</li>';
+    }
+    var ul = document.getElementById('namelist');
+    ul.innerHTML=list;
+}
